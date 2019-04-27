138c2377cd0f121b85c3c4776878ce17973913de
Amal Joseph <amaljoseph13@gmail.com>
2019-04-27 15:15:52 +0530

    Update face_detect.py

--- face_detect.py
+++ face_detect.py
@@ -1,10 +1,11 @@
 import cv2
 import sys
 
-def face_detect(imagePath):	
-	cascPath = "opencv-files/haarcascade_frontalface_alt.xml"
-	faceCascade = cv2.CascadeClassifier(cascPath)
+def face_detect(imagePath):
+	# HaarCascade file, to detect the face.
+	faceCascade = cv2.CascadeClassifier("opencv-files/haarcascade_frontalface_alt.xml")
 	image = cv2.imread(imagePath)
+	#Convert image to grayscale
 	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 	grays = []
 	faces = faceCascade.detectMultiScale(
@@ -13,17 +14,19 @@ def face_detect(imagePath):
 		minNeighbors=5,
 		minSize=(30, 30)
 	)
-
+	
+	# For drawing rectangles over multiple faces in the image
 	for (x, y, w, h) in faces:
 		cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
+	
+	# Show Detected faces.
 	cv2.imshow("Faces found", image)
 	cv2.waitKey(1)
+	
+	# Append the detected faces into grays list.
 	for i in range(0, len(faces)):
 		(x, y, w, h) = faces[i]
 		grays.append(gray[y:y+w, x:x+h])
 	print "------------------------------------------------------------"
 	print "Detecting Face -\-"
-	print grays
-	print "------------------------------------------------------------"
 	return grays, faces, len(faces)
